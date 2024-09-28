9e5872678feb5cde27cf698e48b65e9f5d475c5f
Julian Silvestrini <jsilvestrini2002@gmail.com>
2024-09-28 15:45:11 -0500

    Have a general untested environment

--- er_environment.py
+++ er_environment.py
@@ -6,6 +6,7 @@ from scripts import walk_back
 import dxcam
 import time
 import numpy as np
+import torch
 
 # TODO: Make game access save previous enemy pointers to ensure old pointer is replaced
 
@@ -14,26 +15,34 @@ action_space = {
     1: ['a'],
     2: ['s'],
     3: ['d'],
-    4: ['space'],
-    5: ['f'],
-    6: ['x'],
-    7: ['q'],
-    8: ['3'],
-    9: ['4'],
-    10: ['up'],
-    11: ['left'],
-    12: ['right'],
-    13: ['down'],
-    14: ['m'],
-    15: ['l'],
-    16: ['k'],
-    17: ['n'],
-    18: ['r'],
+    4: ['w', 'space'],
+    5: ['a', 'space'],
+    6: ['s', 'space'],
+    7: ['d', 'space'],
+    8: ['space'],
+    9: ['f'],
+    10: ['w', 'f'],
+    11: ['a', 'f'],
+    12: ['s', 'f'],
+    13: ['d', 'f'],
+    14: ['x'],
+    15: ['3'],
+    16: ['4'],
+    17: ['up'],
+    18: ['left'],
+    19: ['right'],
+    20: ['down'],
+    21: ['m'],
+    22: ['l'],
+    23: ['k'],
+    24: ['n'],
+    25: ['r'],
 }
 
 class EldenRing:
     def __init__(self):
         self.__game = GameAccessor()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.__camera = dxcam.create()
         left, top, right, bottom = er_helper.client_window_size()
         self.__region = (left, top, right, bottom)
@@ -56,6 +65,10 @@ class EldenRing:
             self.player_coords = np.array(self.__player.get_coords())
             self.player_coords_prev = self.player_coords
             self.player_animation = self.__player.get_animation()
+            self.player_animation_list = er_helper.get_animation_files("animation_files/000000")
+            self.player_animation_list_zero = np.zeros_like(self.player_animation_list)
+            self.player_animation_timer = time.time()
+            self.player_previous_animation = self.player_animation
         # list of enemies
         self.enemies = []
         self.boss_health = []
@@ -66,6 +79,10 @@ class EldenRing:
         self.distance_from_player = []
         self.direction_from_player = []
         self.boss_animation = []
+        self.boss_animation_list = []
+        self.boss_animation_list_zero = []
+        self.boss_animation_timers = []
+        self.boss_previous_animation = []
         # reward
         self.reward = 0
         # frame queue
@@ -77,7 +94,11 @@ class EldenRing:
         # boss (or bosses) dead
         self.boss_dead = []
         # walk_back function
-        ...
+        time.sleep(10)
+        walk_back.soldier_of_godrick()
+        self.__game.check()
+        self.enemies = self.__game.get_enemies()
+        self.begin_time = time.time()
 
     def full_reset(self) -> None:
         self.__game.begin_reset()
@@ -88,25 +109,42 @@ class EldenRing:
         index = np.nonzero(action)[0]
         index = index[0]
         print(action_space[index])
+        if index in [15, 16]:
+            self.__game.check()
         er_helper.key_press(action_space[index])
 
+    def encode_player_animation(self):
+        index = np.where(self.player_animation_list, self.player_animation)
+        self.player_animation_list_zero = np.zeros_like(self.player_animation_list)
+        self.player_animation_list_zero[index] = 1
+
+    def encode_enemy_animation(self):
+        for i in range(len(0, self.enemies)):
+            index = np.where(self.boss_animation_list[i], self.boss_animation[i])
+            self.boss_animation_list_zero[i] = np.zeros_like(self.boss_animation_list[i])
+            self.boss_animation_list_zero[i][index] = 1
+
     def updates(self) -> None:
-        self.__game.check()
         self.player_health_prev = self.player_health
         self.player_stamina_prev = self.player_stamina
         self.player_prev_fp = self.player_fp
         self.player_coords_prev = self.player_coords
+        self.player_previous_animation = self.player_animation
         self.player_health = self.__player.get_health()
         self.player_stamina = self.__player.get_stamina()
         self.player_fp = self.__player.get_fp()
         self.player_coords = np.array(self.__player.get_coords())
         self.player_animation = self.__player.get_animation()
+        if self.player_animation != self.player_previous_animation:
+            self.player_animation_timer = time.time()
+        self.encode_player_animation()
         self.player_dead = self.player_health <= 0
 
         self.enemies = self.__game.get_enemies()
 
         self.boss_health_prev = self.boss_health
         self.boss_coords_prev = self.boss_coords
+        self.boss_previous_animation = self.boss_animation
 
         for i in range(0, len(self.enemies)):
             self.boss_health[i] = self.enemies[i].get_health()
@@ -114,6 +152,11 @@ class EldenRing:
             self.boss_coords[i] = np.array(self.enemies[i].get_coords())
             self.boss_animation[i] = self.enemies[i].get_animation()
             self.boss_dead[i] = self.boss_health[i] <= 0
+            self.boss_animation_list[i] = er_helper.get_animation_files("animation_files/{}".format(self.enemies[i].get_id()))
+            self.boss_animation_list_zero[i] = np.zeros_like(self.boss_animation_list)
+            if self.boss_animation[i] != self.boss_previous_animation[i]:
+                self.boss_animation_timers[i] = time.time()
+        self.encode_enemy_animation()
         self.distance_from_player = np.linalg.norm(self.boss_coords - self.player_coords, axis=1)
         self.direction_from_player = np.arctan2(self.boss_coords[:, 1] - self.player_coords[1], self.boss_coords[:, 0] - self.player_coords[0]) * 180 / np.pi
 
@@ -122,17 +165,48 @@ class EldenRing:
     def screenshot(self) -> None:
         # screenshot, append to list, pop front if needed, check if other frame
         if self.screenshot_check:
-            self.frame_stack.append(self.__camera.grab(region = self.__region))
+            self.frame_stack.append(np.array(self.__camera.grab(region = self.__region)))
             if len(self.frame_stack) > 5:
                 self.frame_stack.pop(0)
         self.screenshot_check = not self.screenshot_check
 
-    def get_state(self) -> dict:
-        # dictionary of all states
-        ...
+    def get_state(self):
+        player_health = self.player_health / self.player_max_health
+        player_stamina = self.player_stamina / self.player_max_stamina
+        player_fp = self.player_fp / self.player_max_fp
+        player_animation = self.player_animation_list_zero
+        player_delta_health = (self.player_health_prev - self.player_health) / self.player_max_health
+        player_delta_stamina = (self.player_stamina_prev - self.player_stamina) / self.player_max_stamina
+        player_delta_fp = (self.player_prev_fp - self.player_fp) / self.player_max_fp
+        animation_completion = (self.player_animation_timer - time.time()) / er_helper.max_time("animation_files/000000", self.player_animation)
+        player_tensor = torch.tensor([player_health, player_delta_health, player_stamina, player_delta_stamina, 
+                                      player_fp, player_delta_fp, player_animation, animation_completion], dtype=torch.float32).to(self.device)
+
+        enemy_health = np.array(self.boss_health) / np.array(self.boss_max_health)
+        enemy_delta_health = (np.array(self.boss_health_prev) - np.array(self.boss_health)) / np.array(self.boss_max_health)
+        enemy_animation = self.boss_animation_list_zero
+        enemy_distance = self.distance_from_player
+        enemy_direction = self.direction_from_player
+        enemy_animation_completion = []
+        for i in range(len(self.enemies)):
+            enemy_animation_completion[i] = (self.boss_animation_timers[i] - time.time()) / er_helper.max_time("animation_files/{}".format(self.enemies[i].get_id()), self.boss_animation[i])
+        boss_array = np.column_stack(enemy_health, enemy_delta_health, enemy_distance, enemy_direction, enemy_animation, enemy_animation_completion)
+        enemy_tensor = torch.tensor(boss_array, dtype=torch.float32).to(self.device)
+        frame_tensor = torch.tensor(self.frame_stack, dtype=torch.float32).to(self.device)
+
+        # Combine tensors and return
+        combined_tensor = torch.cat([player_tensor.flatten(), enemy_tensor.flatten(), frame_tensor.flatten()], dim=0)
+        return combined_tensor
+
     def rewards(self) -> None:
-        # calculate reward
-        ...
+        self.reward = 0
+        if len(self.enemies) > 0:
+            for i in range(0, len(self.enemies)):
+                self.reward += ((self.boss_health_prev[i] - self.boss_health[i]) + 1) / self.boss_max_health[i]
+        self.reward -= ((self.player_health_prev - self.player_health) + (self.player_max_health * 0.1)) / self.player_max_health
+        if self.player_stamina < (0.15 * self.player_max_stamina):
+            self.reward -= self.player_stamina / self.player_max_stamina
+        self.reward /= (time.time() - self.begin_time() / 5)
 
     def done(self) -> bool:
         pd = self.player_dead
@@ -147,10 +221,11 @@ class EldenRing:
         # use other functions, check if reset needs to be called
         self.perform_action(action)
         self.updates()
-        self.reward = self.rewards()
+        self.rewards()
         new_state = self.get_state()
         done = self.done()
         return new_state, self.reward, done
 
 if __name__ == "__main__":
-    er = EldenRing()
+    #er = EldenRing()
+    ...
--- scripts/er_helper.py
+++ scripts/er_helper.py
@@ -1,6 +1,9 @@
 import win32gui
 import time
 import pydirectinput
+import numpy as np
+import os
+import json
 
 def find_activate_window() -> None:
     """
@@ -23,6 +26,48 @@ def find_activate_window() -> None:
 def client_window_size() -> list:
     hwnd = win32gui.FindWindow(None, "ELDEN RING™")
     return win32gui.GetClientRect(hwnd)
+
+def get_file_count_and_zero_array(folder_path) -> np.ndarray:
+    """
+    Counts the number of files in a folder and creates a NumPy array of zeros with that size.
+
+    Args:
+        folder_path: The path to the folder.
+
+    Returns:
+        A NumPy array that contains 0s
+    """
+
+    file_count = len([f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))])
+    zero_array = np.zeros(file_count)
+    return zero_array
+
+def get_animation_files(folder_path) -> np.ndarray:
+    """
+    Extracts the numeric parts of file names in a folder and returns them as a NumPy array.
+
+    Args:
+        folder_path: The path to the folder.
+
+    Returns:
+        A NumPy array containing the numeric parts of the file names.
+    """
+
+    file_names = os.listdir(folder_path)
+    file_numbers = []
+    for file_name in file_names:
+        if file_name.endswith('.json'):
+            file_number = int(file_name[:-5])  # Remove '.json' and convert to int
+            file_numbers.append(file_number)
+    return np.array(file_numbers)
+
+def max_time(file_path, animation_number : int) -> float:
+    data = 10000.0
+    if os.path.isfile(file_path + "/{}.json".format(animation_number)):
+        with open(file_path + "/{}.json".format(animation_number), 'r') as file:
+            data = json.load(file)
+            data = data["max_time_to_finish"]
+    return data
 
 def key_presses(keys: list) -> None:
     """
